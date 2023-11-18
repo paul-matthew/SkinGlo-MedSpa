01042ffcf290fbc409f04a951927347bd7f8ed27
Paul Matthew <116613750+paul-matthew@users.noreply.github.com>
2023-11-18 17:29:06 -0500

    Baseline 1.28
    ALTERNATIVE: Update to give client another option to site homepage...added touch button

--- js/main.js
+++ js/main.js
@@ -7,94 +7,93 @@
 		once: true
 	});
 
-	var slider = function(){
-
+	var slider = function () {
 		var heroSlider = document.querySelectorAll('.hero-slider');
-
-		if ( heroSlider.length > 0 ) {
-			var heroSlider = tns({
-				container: '.hero-slider',
-				items: 1,
-				mode: 'carousel',
-				autoplay: true,
-			  animateIn: 'tns-fadeIn',
-		    animateOut: 'tns-fadeOut',
-				speed: 700,
-				nav: true,
-				controls: false,
-				autoplayButtonOutput: false,
-			});
+	  
+		if (heroSlider.length > 0) {
+		  var heroSlider = tns({
+			container: '.hero-slider',
+			items: 1,
+			mode: 'carousel',
+			autoplay: true,
+			animateIn: 'tns-fadeIn',
+			animateOut: 'tns-fadeOut',
+			speed: 700,
+			nav: true,
+			controls: false,
+			autoplayButtonOutput: false,
+			touch: true, // Enable touch/swipe
+		  });
 		}
-
+	  
 		var carouselCourses = document.querySelectorAll('.carousel-courses');
-		if ( carouselCourses.length > 0 ) {
-
-			var coursesSlider = tns({
-				container: '.carousel-courses',
+		if (carouselCourses.length > 0) {
+		  var coursesSlider = tns({
+			container: '.carousel-courses',
+			items: 1,
+			mode: 'carousel',
+			autoplay: true,
+			animateIn: 'tns-fadeIn',
+			animateOut: 'tns-fadeOut',
+			speed: 700,
+			nav: true,
+			gutter: 20,
+			controls: false,
+			autoHeight: true,
+			autoplayButtonOutput: false,
+			touch: true, // Enable touch/swipe
+			responsive: {
+			  0: {
 				items: 1,
-				mode: 'carousel',
-				autoplay: true,
-			  animateIn: 'tns-fadeIn',
-		    animateOut: 'tns-fadeOut',
-				speed: 700,
-				nav: true,
+				gutter: 0,
+			  },
+			  600: {
+				items: 2,
 				gutter: 20,
-				controls: false,
-				autoHeight: true,
-				autoplayButtonOutput: false,
-				responsive:{
-					0:{
-						items: 1,
-						gutter: 0
-					},
-					600:{
-						items: 2,
-						gutter: 20
-					},
-					1000:{
-						items: 3,
-						gutter: 20
-					}
-				}
-			});
-
+			  },
+			  1000: {
+				items: 3,
+				gutter: 20,
+			  },
+			},
+		  });
 		}
-
+	  
 		var carouselSlider = document.querySelectorAll('.carousel-testimony');
-		if ( carouselSlider.length > 0 ) {
-
-			var testimonySlider = tns({
-				container: '.carousel-testimony',
+		if (carouselSlider.length > 0) {
+		  var testimonySlider = tns({
+			container: '.carousel-testimony',
+			items: 1,
+			mode: 'carousel',
+			autoplay: true,
+			animateIn: 'tns-fadeIn',
+			animateOut: 'tns-fadeOut',
+			speed: 700,
+			nav: true,
+			gutter: 20,
+			controls: false,
+			autoplayButtonOutput: false,
+			touch: true, // Enable touch/swipe
+			responsive: {
+			  0: {
 				items: 1,
-				mode: 'carousel',
-				autoplay: true,
-			  animateIn: 'tns-fadeIn',
-		    animateOut: 'tns-fadeOut',
-				speed: 700,
-				nav: true,
+				gutter: 0,
+			  },
+			  600: {
+				items: 2,
 				gutter: 20,
-				controls: false,
-				autoplayButtonOutput: false,
-				responsive:{
-					0:{
-						items: 1,
-						gutter: 0
-					},
-					600:{
-						items: 2,
-						gutter: 20
-					},
-					1000:{
-						items: 3,
-						gutter: 20
-					}
-				}
-			});
-
+			  },
+			  1000: {
+				items: 3,
+				gutter: 20,
+			  },
+			},
+		  });
 		}
-
-	}
-	slider();
+	  };
+	  
+	  slider();
+	  
 	
 	//COUNTER
 	'use trict';
